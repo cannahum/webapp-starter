742b363a7f02488d8582aa90942e3c60e103acfc
Can Nahum <cannahum@outlook.com>
2018-04-05 23:23:57 +0300

    SAVED

--- src/client/example_components/App.test.tsx
+++ src/client/example_components/App.test.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-dom/test-utils';
-import App from "./App";
+import {configure, shallow, ShallowWrapper} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import App from './App';
+import AsyncComp from './AsyncComp';
+
+configure({ adapter: new Adapter() });
 
 describe('<App/> tests', () => {
   test('The component gets rendered', () => {
     const app = TestUtils.renderIntoDocument(
-      <App/>
+      <App/>,
     ) as React.Component<any, any>;
     const appNode: Node = ReactDOM.findDOMNode(app);
     expect(appNode).toBeDefined();
+  });
+
+  test('<App/> has <AsyncComp/> inside', () => {
+    const wrapper: ShallowWrapper<App> = shallow(<App/>);
+    expect(wrapper.find(AsyncComp)).toHaveLength(1);
   });
 });
