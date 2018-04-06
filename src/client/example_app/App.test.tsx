b3b1703df1dd992ae8e82e0983c8abb1eee38198
Can Nahum <cannahum@outlook.com>
2018-04-06 03:32:36 +0300

    Tests are back to normal, deleted <Link/> because it needs redux store mocking.

--- src/client/example_app/App.test.tsx
+++ src/client/example_app/App.test.tsx
@@ -3,15 +3,17 @@ import ReactDOM from 'react-dom';
 import TestUtils from 'react-dom/test-utils';
 import {configure, shallow, ShallowWrapper} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import App from './App';
 import AsyncComp from './components/AsyncComp';
+import {Provider} from 'react-redux';
+import App from './App';
+import {store} from './redux/index';
 
 configure({ adapter: new Adapter() });
 
 describe('<App/> tests', () => {
   test('The component gets rendered', () => {
     const app = TestUtils.renderIntoDocument(
-      <App/>,
+      <Provider store={store}><App/></Provider>,
     ) as React.Component<any, any>;
     const appNode: Node = ReactDOM.findDOMNode(app);
     expect(appNode).toBeDefined();
