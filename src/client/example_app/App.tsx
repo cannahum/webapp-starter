a625c897aa89c550975f20a04358ebb524978523
Can Nahum <cannahum@outlook.com>
2018-04-06 03:35:32 +0300

    example app complete.

--- src/client/example_app/App.tsx
+++ src/client/example_app/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import AsyncComp from './components/AsyncComp';
 import Counter from './components/CounterConnected';
 import Link from './components/Link';
+import './example.scss';
 
 export default class App extends React.Component<{}, {}> {
   public render() {
     return (
-      <div id="main-app-wrapper">
+      <div id="example-app-wrapper">
         <h1>Welcome To The Full Stack App Starter!</h1>
         <AsyncComp/>
         <div>
