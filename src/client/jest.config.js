e7b34d4e419695c2814475bb798d00485c017f81
Can Nahum <cannahum@outlook.com>
2018-04-21 13:19:42 +0300

    jsx tests work correctly.

--- src/client/jest.config.js
+++ src/client/jest.config.js
@@ -10,7 +10,10 @@ module.exports = {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
     "\\.(css|scss|sass)$": "<rootDir>/__mocks__/styleMock.js"
   },
-  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$",
+  "testMatch": [
+    "**/src/**/*.test.jsx",
+    "**/src/client/**/*.test.tsx"
+  ],
   "moduleFileExtensions": [
     "ts",
     "tsx",
