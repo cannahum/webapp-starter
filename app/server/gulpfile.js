4c7026af09f33c39bd7f413224e2938a1a34e89b
Can Nahum <cannahum@outlook.com>
2018-05-19 11:23:11 +0300

    Got distracted by gulp-nodemon. Works well though! Now we can live reload the server code.

--- app/server/gulpfile.js
+++ app/server/gulpfile.js
@@ -1,17 +1,26 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const sourcemaps = require('gulp-sourcemaps');
+const nodemon = require('gulp-nodemon');
 
 const tsProject = ts.createProject('tsconfig.json');
 
 gulp.task('compile-server', () => {
+  console.log('Compiling Server code.');
   return tsProject.src()
     .pipe(sourcemaps.init())
     .pipe(tsProject(ts.reporter.fullReporter()))
     .pipe(sourcemaps.write('.', {sourceRoot: './', includeContent: false}))
     .pipe(gulp.dest('dist'));
-
 });
-gulp.task('watch-typescript', () => {
-  gulp.watch('src/**/*', ['compile-server']);
+
+gulp.task('watch-typescript', ['compile-server'],  () => {
+  const stream = nodemon({
+    script: 'bin/dev',
+    ext: '*',
+    watch: 'src/**/*',
+    tasks: ['compile-server']
+  });
+  return stream;
+  // gulp.watch('src/**/*', ['compile-server']);
 });
